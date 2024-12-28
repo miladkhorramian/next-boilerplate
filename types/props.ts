56b97894122f75abd1173f4f5f8d94c56ffc4694
Milad Khorramian <mahdiar.khorramian@yahoo.com>
2024-12-28 15:18:32 +0330

    NEW DIRECTORY

--- types/props.ts
+++
@@ -1,6 +0,0 @@
-export interface LayoutProps {
-  children: React.ReactNode;
-}
-
-// eslint-disable-next-line @typescript-eslint/no-empty-object-type
-export interface RootLayoutProps extends LayoutProps {}
